d634d95a6ceb1228448f295bc5e2536ef7e5a76b
ifigurin12 <ifigurin1411@gmail.com>
2023-06-05 00:43:31 +0300

    add: add firebase in apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:musically_street/ui/pages/listeners/musicant_detail.dart';
 import 'package:musically_street/ui/pages/listeners/performance_detail.dart';
 import 'package:musically_street/ui/pages/musicants/booking.dart';
 import 'package:musically_street/ui/pages/musicants/profile.dart';
 
-void main() {
+// Указать авторство иконки 
+
+Future<void> main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+     await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(PerformanceDetail());
 }
 
---
+++ lib/ui/pages/start.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class StartPage extends StatefulWidget {
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> 
+{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+  
+}
